89d78e4c95d3de4a32df7dcb46cf006e649bb9a3
joe <donegandiarmuid@gmail.com>
2026-04-18 02:53:10 +0000

    ● Done. set debuginfod enabled on pre-answers yes so the prompt never appears. ● Done. set print thread-events off suppresses the [New Thread ...] / [Thread ... exited] notifications.

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ export BUILD_DIR="../build-ragbot"
 export CODE_DIR=$(pwd)
 cp ./config.json $BUILD_DIR
 cd $BUILD_DIR
-gdb --args ./ragbot -c ./config.json
+gdb -ex "set debuginfod enabled on" -ex "set print thread-events off" --args ./ragbot -c ./config.json
 cd $CODE_DIR
