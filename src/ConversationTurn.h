949cc9f1434d6f577f7a6d6241f5daff332b6668
joe <donegandiarmuid@gmail.com>
2026-04-26 20:04:06 +0000

    remove history of database files

---
+++ src/ConversationTurn.h
@@ -0,0 +1,12 @@
+#ifndef CONVERSATIONTURN_H
+#define CONVERSATIONTURN_H
+
+#include <QString>
+
+struct ConversationTurn {
+    QString question;
+    QString researchAnswer;
+    QString roleplayAnswer;
+};
+
+#endif // CONVERSATIONTURN_H
